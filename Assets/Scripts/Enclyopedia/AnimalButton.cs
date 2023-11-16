c06ec65d02be8dcaa047bbf9e8ea8b24bcc730d0
Nadvi <nadvi019zaman@gmail.com>
2023-11-16 12:05:26 +0000

    can make a grid of buttons

---
+++ Assets/Scripts/Enclyopedia/AnimalButton.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class AnimalButton : MonoBehaviour
+{
+   
+
+    private AnimalAns[] allAnimals;
+    private AnimalAns animalData;
+
+    public TMP_Text nameText; // Reference to the Text component displaying the animal's name
+
+    public void SetAnimalData(AnimalAns data)
+    {
+        animalData = data;
+        nameText.text = data.endangeredStatus;
+    }
+
+    public void OnButtonClick()
+    {
+        // Implement logic to show more information about the clicked animal
+        Debug.Log($"Clicked on {animalData.name}. Description: {animalData.endangeredStatus}");
+    }
+}
---
+++ Assets/Scripts/Enclyopedia/EnclyclopediaManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncyclopediaManager : MonoBehaviour
+{
+    public GameObject animalButtonPrefab;
+    public Transform gridParent;
+    public AnimalAns[] animalDataArray;
+    public List<AnimalAns> myAnimalFactsList = new List<AnimalAns>();
+    public TextAsset answerBankText;
+
+    void Start()
+    {
+        PopulateAnimalGrid();
+    }
+
+    void PopulateAnimalGrid()
+    {
+        readCSV();
+        foreach (var animalData in myAnimalFactsList)
+        {
+
+            GameObject buttonInstance = Instantiate(animalButtonPrefab, gridParent);
+            AnimalButton animalButton = buttonInstance.GetComponent<AnimalButton>();
+            animalButton.SetAnimalData(animalData);
+        }
+    }
+
+    public void readCSV()
+    {
+        //reads CSV into string
+        string[] data = answerBankText.text.Split(new[] { ",", "\n" }, StringSplitOptions.None);
+        int numOfAnimal = data.Length / 12 - 1;
+
+        for (int i = 0; i < numOfAnimal; i++)
+        {
+            int offset = 12 * i;
+
+
+            //writes csv into datastructure
+            AnimalAns newAnimal = new AnimalAns();
+
+            newAnimal.name = data[offset];
+            newAnimal.endangeredStatus = data[offset + 1];
+            newAnimal.latinName = data[offset + 2];
+            newAnimal.diet = data[offset + 3];
+            newAnimal.wildAge = data[offset + 4];
+            newAnimal.captivAge = data[offset + 5];
+            newAnimal.weight = data[offset + 6];
+            newAnimal.anLength = data[offset + 7];
+            newAnimal.anheight = data[offset + 8];
+            newAnimal.offspringNum = data[offset + 9];
+            newAnimal.predators = data[offset + 10];
+            newAnimal.funFact = data[offset + 11];
+
+            myAnimalFactsList.Add(newAnimal);
+        }
+    }
+}
