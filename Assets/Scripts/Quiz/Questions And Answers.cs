d0e2d1874fd93bd24fee26018ea97f6893be05f1
Nadvi <nadvi019zaman@gmail.com>
2023-11-22 16:43:36 +0000

    fixed diet q glitch

--- Assets/Scripts/Quiz/Questions And Answers.cs
+++ Assets/Scripts/Quiz/Questions And Answers.cs
@@ -6,7 +6,7 @@ using System.Xml.Serialization;
 using UnityEngine;
 using static UnityEditor.PlayerSettings;
 
-public class QuestionsAndAnswers : MonoBehaviour 
+public class QuestionsAndAnswers : MonoBehaviour
 {
     //save questions
     public string Question;
@@ -27,13 +27,13 @@ public class QuestionsAndAnswers : MonoBehaviour
         int indexNeeded = 0;
         //finds where animal is that will be asked about 
         for (int i = 0; i < myAnimalFactsList.Count; i++)
-        {   
+        {
             if (myAnimalFactsList[i].name == animalInteracted)
             {
                 indexNeeded = i;
             }
         }
-   
+
         //find 3 non related animals to deceive
         List<int> possibleIndices = Enumerable.Range(1, myAnimalFactsList.Count).ToList();
         possibleIndices.Remove(indexNeeded);
@@ -59,30 +59,39 @@ public class QuestionsAndAnswers : MonoBehaviour
         }
 
         //choses a question to ask from available
-        int QuestionToAsk = UnityEngine.Random.Range(1, QuestionsAvailable[questionsToPick-1]);
-        
+        int QuestionToAsk = UnityEngine.Random.Range(1, QuestionsAvailable[questionsToPick - 1]);
         //set question
         switch (QuestionToAsk)
         {
-            case 1: Question = "What is this animal's conservation status";
+            case 1:
+                Question = "What is this animal's conservation status";
                 break;
-            case 2: Question = "What is this animal's latin name?";
+            case 2:
+                Question = "What is this animal's latin name?";
                 break;
-            case 3: Question = "What type of diet does this animal follow?";
+            case 3:
+                Question = "What type of diet does this animal follow?";
                 break;
-            case 4: Question = "What is the average lifespan for this animal in the WILD?";
+            case 4:
+                Question = "What is the average lifespan for this animal in the WILD?";
                 break;
-            case 5: Question = "What is the average lifespan for this animal in CAPTIVITY?";
+            case 5:
+                Question = "What is the average lifespan for this animal in CAPTIVITY?";
                 break;
-            case 6: Question = "What is the average WEIGHT for this animal?";
+            case 6:
+                Question = "What is the average WEIGHT for this animal?";
                 break;
-            case 7: Question = "What is the average LENGTH for this animal?";
-                break; 
-            case 8: Question = "What is the average HEIGHT for this animal?"; 
+            case 7:
+                Question = "What is the average LENGTH for this animal?";
                 break;
-            case 9: Question = "How many offspring does the animal have on average?";
-                break;  
-            case 10: Question = "Who is this animal's predators?";
+            case 8:
+                Question = "What is the average HEIGHT for this animal?";
+                break;
+            case 9:
+                Question = "How many offspring does the animal have on average?";
+                break;
+            case 10:
+                Question = "Who is this animal's predators?";
                 break;
         }
 
@@ -94,26 +103,51 @@ public class QuestionsAndAnswers : MonoBehaviour
         string attributeName = myAttributes[QuestionToAsk];
         FieldInfo field = typeof(AnimalAns).GetField(attributeName);
         
+        //forces 3 answers boxes for diet q
+        string[] incorrectDiet = {"Herbivore", "Omnivore", "Carnivore", "Herbivore", "Omnivore", "Carnivore" };
+        List<string> stringList = new List<string>(incorrectDiet);
+        if(QuestionToAsk == 3)
+        {
+            stringList.Remove(myAnimalFactsList[indexNeeded].ToString());
+            incorrectDiet = stringList.ToArray();
+        }
+
         //fills up all 4 answers
         for (int i = 0; i < 4; i++)
-        {    
+        {
             //puts away right answer in correct answer spot
             if (i == CorrectAnswer)
             {
                 object attributeValue = field.GetValue(myAnimalFactsList[indexNeeded]);
-                Debug.Log(attributeValue);  
                 Answers[i] = attributeValue?.ToString();
             }
             else
             {
-                //puts wrong answers in extra answer spots
-                object attributeValue = field.GetValue(myAnimalFactsList[incorrectAnimals[wrongAniDone]]);
-                if (!(Answers.Contains(attributeValue.ToString())))
+                if(QuestionToAsk == 3)
                 {
-                    Answers[i] = attributeValue?.ToString();
+                    Debug.Log("String Array: " + string.Join(", ", Answers));
+                    //if diet situation
+                    string correctAns = field.GetValue(myAnimalFactsList[indexNeeded]).ToString();
+                    Debug.Log(incorrectDiet[wrongAniDone]);
+                    //if not already added/ correct answer so no duplicates
+                    if (!(Array.Exists(Answers, element => element == incorrectDiet[wrongAniDone])) && !(string.Equals(incorrectDiet[wrongAniDone],correctAns, StringComparison.Ordinal)))
+                    {
+                        Debug.Log("Added");
+                        Answers[i] = incorrectDiet[wrongAniDone];
+                    }
                     wrongAniDone++;
-                }    
-                           
+                }
+                else
+                {
+                    //puts wrong answers in extra answer spots
+                    object attributeValue = field.GetValue(myAnimalFactsList[incorrectAnimals[wrongAniDone]]);
+                    if (!(Answers.Contains(attributeValue.ToString())))
+                    {
+                        Answers[i] = attributeValue?.ToString();
+                        wrongAniDone++;
+                    }
+                }                       
+
             }
         }
         return CorrectAnswer;
@@ -127,7 +161,7 @@ public class QuestionsAndAnswers : MonoBehaviour
 
         for (int i = 0; i < numOfAnimal; i++)
         {
-                int offset = 12 * i;
+            int offset = 12 * i;
 
 
             //writes csv into datastructure
@@ -149,7 +183,6 @@ public class QuestionsAndAnswers : MonoBehaviour
             myAnimalFactsList.Add(newAnimal);
         }
 
-       
+
     }
 }
-
