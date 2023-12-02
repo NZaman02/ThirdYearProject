8a479be35c6da6d17f376643bbbb52fbd28d7480
Nadvi <nadvi019zaman@gmail.com>
2023-12-02 15:09:53 +0000

    added cam zoom

--- Assets/Scripts/OpenWorld/CameraFollow.cs
+++ Assets/Scripts/OpenWorld/CameraFollow.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
     public float FollowSpeed = 2f;
     public Transform target;
+    public float minZoom = 2f;
+    public float maxZoom = 10f;
+    public float zoomSpeed = 5f;
+    private float zoomInput = 0f;
+
+
 
     // Update is called once per frame
     void Update()
     {
         Vector3 newPos  = new Vector3(target.position.x, target.position.y, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+
+       
+        if (Input.GetKey(KeyCode.O))
+        {
+            zoomInput = 1f;
+        }
+        else if (Input.GetKey(KeyCode.P))
+        {
+            zoomInput = -1f;
+        }
+
+        ZoomCamera(zoomInput);
+    }
+
+    void ZoomCamera(float zoomInput)
+    {
+        Camera.main.orthographicSize -= zoomInput * zoomSpeed * Time.deltaTime;
+
+        // Clamp the orthographic size to avoid extreme zoom levels
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minZoom, maxZoom);
+
     }
 }
